6816511f5856c1363693a4f82a2c56dc97a5eb52
Jorge <jorge.valdivia1901@alumnos.ubiobio.cl>
2023-07-28 18:06:08 -0400

    Muestra de Fechas en el calendario base.

--- frontend/src/app/asistencias/page.jsx
+++ frontend/src/app/asistencias/page.jsx
@@ -1,14 +1,14 @@
 'use client';
-import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import CardHeader from "@mui/material/CardHeader";
-import Grid from "@mui/material/Grid";
-import Navbar from "@/components/Navbar";
-import api from "@/api/rootAPI";
-import { useState, useEffect } from "react";
-
+import React from 'react';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardHeader from '@mui/material/CardHeader';
+import Grid from '@mui/material/Grid';
+import Navbar from '@/components/Navbar';
+import api from '@/api/rootAPI';
+import { useState, useEffect } from 'react';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -17,27 +17,30 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import Link from 'next/link';
-
-import CalendarAttendance from "@/components/CalendarAttendance";
-import AttendanceModal from "@/components/AttendanceModal"; // Importa el componente de modal
+import CalendarAttendance from '@/components/CalendarAttendance';
 
 export default function Page() {
-    const [asistencias, setAsistencias] = useState([])
+    const [asistencias, setAsistencias] = useState([]);
 
-    const fetchData = async() => {
-        const res = await api.get('/asistencias')
-        console.log(res.data.attendances)
-        return res.data.attendances
-    }
-    useEffect (()=>{
-        fetchData().then(res => setAsistencias(res))
-    }, [])
+    const fetchData = async () => {
+        const res = await api.get('/asistencias');
+        console.log(res.data.attendances);
+        return res.data.attendances;
+    };
+
+    useEffect(() => {
+        fetchData().then(res => setAsistencias(res));
+    }, []);
+
     return (
         <>
         <Navbar />
-        <div>
-            <Typography variant="h1">Hello, Asistencias page!</Typography>
-            <Grid container spacing={2}>
+        <div style={{ padding: '20px' }}>
+            <Typography variant="h1">Sistema de Asistencias</Typography>
+            <Typography variant="h5" gutterBottom>
+            Bienvenido a nuestro sistema de asistencias. Aquí puedes registrar y ver las asistencias de los brigadistas.
+            </Typography>
+            <Grid container spacing={2} style={{ marginTop: '20px' }}>
             <Grid item xs={12} sm={6}>
                 <Card>
                 <CardHeader title="Registrar Asistencia" />
@@ -46,7 +49,7 @@ export default function Page() {
                     Aquí puedes registrar una nueva asistencia.
                     </Typography>
                     <Button variant="contained" color="primary">
-                        <Link href='asistencias/registrar'>Registrar Nueva Asistencia</Link>
+                    <Link href="asistencias/registrar">Registrar Nueva Asistencia</Link>
                     </Button>
                 </CardContent>
                 </Card>
@@ -55,24 +58,21 @@ export default function Page() {
                 <Card>
                 <CardHeader title="Ver Asistencias" />
                 <CardContent>
-                <TableContainer component={Paper}>
+                    <TableContainer component={Paper}>
                     <Table sx={{ minWidth: 650 }} aria-label="simple table">
                         <TableHead>
                         <TableRow>
-                            <TableCell align="right">Brigadista</TableCell>
-                            <TableCell align="right">fecha</TableCell>
-                            <TableCell align="right">Tipo de marca</TableCell>
+                            <TableCell>Brigadista</TableCell>
+                            <TableCell align="center">Fecha</TableCell>
+                            <TableCell align="center">Tipo de Marca</TableCell>
                         </TableRow>
                         </TableHead>
                         <TableBody>
-                        {asistencias?.map((asistencia) => (
-                            <TableRow
-                            key={asistencia._id}
-                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                            >
-                            <TableCell align="right">{asistencia.brigadista.name}</TableCell>
-                            <TableCell align="right">{asistencia.date}</TableCell>
-                            <TableCell align="right">{asistencia.markType}</TableCell>
+                        {asistencias?.map(asistencia => (
+                            <TableRow key={asistencia._id}>
+                            <TableCell>{asistencia.brigadista.name}</TableCell>
+                            <TableCell align="center">{asistencia.date}</TableCell>
+                            <TableCell align="center">{asistencia.markType}</TableCell>
                             </TableRow>
                         ))}
                         </TableBody>
@@ -83,20 +83,14 @@ export default function Page() {
             </Grid>
             </Grid>
         </div>
-        <div>
+        <div style={{ marginTop: '20px' }}>
+            <CalendarAttendance eventos={asistencias} />
         </div>
-        <div>
-        {/* Componente del calendario con los eventos y evento para agregar un nuevo evento */}
-        <CalendarAttendance eventos={eventos} onDateClick={(date) => handleOpenModal(date)} />
-        </div>
-            {/* Modal para registrar asistencia */}
-            <AttendanceModal 
-            isOpen={modalOpen}
-            onClose={handleCloseModal}
-            selectedDate={selectedDate}
-            onSaveAttendance={handleSaveAttendance} 
-        />
-    </>
+        <footer style={{ marginTop: '20px', textAlign: 'center' }}>
+            <Typography variant="body2">
+            © {new Date().getFullYear()} Brigadistas Unidos
+            </Typography>
+        </footer>
+        </>
     );
-}
-
+    }
--- frontend/src/components/CalendarAttendance.jsx
+++ frontend/src/components/CalendarAttendance.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import FullCalendar from '@fullcalendar/react';
-import dayGridPlugin from '@fullcalendar/daygrid'; // a plugin!
-// Importa las traducciones al español
+import dayGridPlugin from '@fullcalendar/daygrid';
 import esLocale from '@fullcalendar/core/locales/es';
 
-    export default class CalendarAttendance extends React.Component {
-    render() {
-        const { eventos } = this.props; // Obtén los eventos desde las props
-        return (
+    export default function CalendarAttendance({ eventos }) {
+    // Convierte las fechas al formato adecuado que FullCalendar espera
+    const eventosFormateados = eventos.map(asistencia => ({
+        title: 'Asistencia', // Puedes personalizar el título del evento como desees
+        start: asistencia.date, // Utiliza la fecha del backend como la fecha del evento
+    }));
+
+    return (
         <FullCalendar
-            plugins={[dayGridPlugin]}
-            initialView="dayGridMonth"
-            locale={esLocale}
-            events={eventos} // Pasa el array de eventos al componente FullCalendar
+        plugins={[dayGridPlugin]}
+        initialView="dayGridMonth"
+        locale={esLocale}
+        events={eventosFormateados} // Pasa el array de eventos formateados al componente FullCalendar
         />
-        );
-    }
+    );
     }
